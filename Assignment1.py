2192c063cdb9f7a34a6e0859084ac4786c23984a
Suraj Jadhao <suraj.jadhao3500@gmail.com>
2026-04-19 17:07:23 +0530

    Demonstrating different data types
    Add Python program demonstrating different data types

---
+++ Assignment1.py
@@ -0,0 +1,49 @@
+# Demonstration of different Python Data Types
+
+#Integer
+integer_value = 10
+print("Integer Value:", integer_value)
+print("Type:", type(integer_value))
+print()
+
+#Float
+float_value = 10.5
+print("Float Value:", float_value)
+print("Type:", type(float_value))
+print()
+
+#String
+string_value = "Hello Python"
+print("String Value:", string_value)
+print("Type:", type(string_value))
+print()
+
+#Boolean
+boolean_value = True
+print("Boolean Value:", boolean_value)
+print("Type:", type(boolean_value))
+print()
+
+#List
+list_value = [1, 2, 3, "Python", 5.5]
+print("List Value:", list_value)
+print("Type:", type(list_value))
+print() 
+
+#Tuple
+tuple_value = (1, 2, 3, "Data")
+print("Tuple Value:", tuple_value)
+print("Type:", type(tuple_value))
+print()
+
+#Dictionary
+dictionary_value = {"Name": "Suraj", "Age":18, "Course": "Computer Science"}
+print("Dictionary Value:", dictionary_value)
+print("Type:", type(dictionary_value))
+print()
+
+#Set
+set_value = {1, 2, 3, 4, 4, 5}
+print("Set Value:", set_value)
+print("Type:", type(set_value))
+print()
