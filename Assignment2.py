b6f2112bcda1ac96c6d036b7e1bad72d8f8b5651
Suraj Jadhao <suraj.jadhao3500@gmail.com>
2026-06-22 08:47:54 +0530

    Refactor Assignment2.py with new examples

--- Assignment2.py
+++ Assignment2.py
@@ -1,32 +1,30 @@
-a = int(input("Enter the value of a = "))
-b = int(input("Enter the value of b = "))
+#1. WAP to add two numbers in python 
+a=10
+b=20
+sum=a+b
+print("Sum of a and b is:", sum)
 
-#Arithmetic Operators
+#2. WAP to declare variables and display types of respective variables 
+a=10
+b=20.5
+c="Hello"
+print("Type of a is:", type(a))
+print("Type of b is:", type(b))
+print("Type of c is:", type(c))
 
-#Addition
-c = a + b
-print(c)
+#3. WAP to demonstrate type casting in python 
+a=10.5
+print(type(a))
+print(int(a))
 
-#Subtraction
-c = a - b 
-print(c)
+b=20
+print(type(b))
+print(float(b))
 
-#Multiplication
-c = a * b
-print(c)
-
-#Division
-c = a / b
-print(c)
-
-#Floor Division
-c = a // b
-print(c)
-
-#Modulus
-c = a % b
-print(c)
-
-#Exponentiation
-c = a ** b
-print(c)
+#4. WAP to demonstrate Logical operators 
+a=True
+b=False
+print("a and b is:", a and b)
+print("a or b is:", a or b)
+print("not a is:", not a)
+print("not b is:", not b)
