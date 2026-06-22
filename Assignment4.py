637658e13aada3844008ea94ff932807ea75a163
Suraj Jadhao <suraj.jadhao3500@gmail.com>
2026-06-22 09:37:17 +0530

    Refactor and simplify number and string operations

--- Assignment4.py
+++ Assignment4.py
@@ -1,35 +1,22 @@
-# Program to display even numbers from 1–10
-
-print("Even numbers from 1 to 10:")
-for i in range(1, 11):
-    if i % 2 == 0:
+#1. WAP to display even numbers from 1-10 
+for i in range (1,11):
+    if i%2==0:
         print(i)
 
-# Program to display odd numbers from 1–10
+#2. WAP to add odd numbers from 1-10
+for i in range (1,11):
+    if i%2!=0:
+        print(i)
 
-print("Sum of Odd numbers from 1 to 10:")
-sum = 0
-for i in range(1, 11):
-    if i % 2 != 0:
-        sum = sum + i
-print("Sum of odd numbers:", sum)
-
-# Program to display Fibonacci series up to 50
-
-print("Fibonacci Series up to 50:" )
-a = 0
-b = 1
-print("Fibonacci Series:")
-
+#3. Write a Python program to get the Fibonacci series between 0 to 50. 
+a=0
+b=1
 while a <= 50:
     print(a)
-    c = a + b
-    a = b
-    b = c
+    c=a+b
+    a=b
+    b=c
 
-# Program to remove characters from odd index values in a string
-
-print("String after removing odd index characters:")
-text = input("Enter a string: ")
-result = text[0::2]
-print("String after removing odd index characters:", result)
+#4. Write a Python program to remove the characters which have odd index valuesof a given string.
+s="Python"
+print(s[::2])
