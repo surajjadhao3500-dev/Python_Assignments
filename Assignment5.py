e0a59248687b7e76f8c669236d7116aac046dab0
Suraj Jadhao <suraj.jadhao3500@gmail.com>
2026-06-22 09:43:50 +0530

    Refactor string manipulation tasks in Assignment5.py

--- Assignment5.py
+++ Assignment5.py
@@ -1,27 +1,12 @@
-# 1. WAP to Calculate length of string
+#1. WAP to Calculate length of string 
+s=input("Enter a string:")
+print(len(s))
 
-str1 = input("Enter a string: ")
+#2. WAP to make string from 1st two and last two characters from given string. 
+s=input("Enter a string:")
+print(s[:2]+s[-2:])
 
-length = len(str1)
-print("Length of string is:", length)
-
-
-# 2. WAP to make string from 1st two and last two characters from given string
-
-str1 = input("Enter a string: ")
-
-if len(str1) < 2:
-    print("String is too short")
-else:
-    result = str1[:2] + str1[-2:]
-    print("New string is:", result)
-
-
-# 3. WAP to concatenate two strings by python
-
-str1 = input("Enter first string: ")
-str2 = input("Enter second string: ")
-
-result = str1 + str2
-
-print("Concatenated string is:", result)
+#3. WAP to concatenate two strings
+a=input("Enter first string:")
+b=input("Enter second string:")
+print(a+b)
